54b0e5493a9479ff07e3ea2743c1fca2cb29883d
PleaBanshee <llewellynant@gmail.com>
2021-07-28 14:08:44 +0200

    Changed looks of searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input
-        className='pa3 ba b--green bg-lightest-blue'
+        className='pa2 mb3 ba b--green bw2 bg-lightest-blue'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
