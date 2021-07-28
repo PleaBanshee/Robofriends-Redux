662599266cbfda4bfc27e3df0d43471b2fbc7104
PleaBanshee <llewellynant@gmail.com>
2021-07-28 22:26:47 +0200

    Added code to avoid unnecessary rerenders

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import CounterButton from '../components/CounterButton';
 
 function Header() {
-    return <h1 className="mb3 f1">Robofriends</h1>;
+    console.log('Hello');
+    return (
+        <div>
+            <h1 className="mb3 f1">Robofriends</h1>
+            <CounterButton color={'red'}/>
+        </div>
+    );
 }
 
-export default Header;
+export default React.memo(Header); // React.memo keeps a component from rerendering. Alternative for class componets is shouldComponentUpdate()
