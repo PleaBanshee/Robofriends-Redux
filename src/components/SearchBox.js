44f9ed4ed7b7ee4af72e7dc9a16d38994f7bc3a2
PleaBanshee <llewellynant@gmail.com>
2021-07-31 18:53:35 +0200

    Added some optimizations

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input
+        aria-label='search robots' /*  define a string that labels the current element. Use where a text label is not visible on the screen */
         className='pa2 mb3 ba b--green bw2 bg-lightest-blue'
         type='search'
         placeholder='search robots'
